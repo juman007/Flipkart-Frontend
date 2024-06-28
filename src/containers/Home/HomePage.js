efc54d98ab4ad60ff1fd2d16bbcc4373e15ef350
Juman Saikia <jumansaikia10@gmail.com>
2024-06-28 17:23:40 +0530

    update color

--- src/containers/Home/HomePage.js
+++ src/containers/Home/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import MenuHeader from "../MenuHeader/MenuHeader";
 
 const HomePage = () => {
    return (
       <div>
          <Header />
+         <MenuHeader />
       </div>
    );
 };
---
+++ src/containers/MenuHeader/MenuHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./menuHeader.css";
+
+const MenuHeader = () => {
+    return <div className="menu-header">
+       
+   </div>;
+};
+
+export default MenuHeader;
